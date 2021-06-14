9bdb6a88b6a77981605c7293dffdd25f101229cd
Andrew <a8yee@ucsd.edu>
2021-06-13 22:19:09 -0700

    First

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+
 database_filename = "database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
@@ -40,7 +41,7 @@ def db_drop_and_create_all():
     )
 
 
-drink.insert()
+# drink.insert()
 # ROUTES
 
 '''

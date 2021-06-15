2dae5ccd925e899822df27453f1a86c1af347c3f
Andrew <a8yee@ucsd.edu>
2021-06-14 21:17:13 -0700

    Solved dependency issues

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -1,13 +1,12 @@
 import json
-from flask import request, _request_ctx_stack
+from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'coffeeshop12345.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'menu'
 
 ## AuthError Exception
 '''
@@ -31,7 +30,16 @@ class AuthError(Exception):
     return the token part of the header
 '''
 def get_token_auth_header():
-   raise Exception('Not Implemented')
+    if 'Authorization' not in request.headers:
+        abort(401)
+
+    auth_header = request.headers['Authorization']
+    header_parts = auth_header.split(' ')
+
+    if len(header_parts) != 2 or header_parts[0].lower() != 'bearer':
+        abort(401)
+
+    raise header_parts[1]
 
 '''
 @TODO implement check_permissions(permission, payload) method
